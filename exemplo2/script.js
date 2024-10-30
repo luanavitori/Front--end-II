97aa1590bbd4e5c4a4850e7679e8348d00ea9e1d
luanavitori <luanavha2007@gmail.com>
2024-10-30 08:25:34 -0400

    aaa

---
+++ exemplo2/script.js
@@ -0,0 +1,36 @@
+const p = document.querySelector('#paragrafo');
+const divImg = document.querySelector('#divImg');
+
+divImg.addEventListener('click', () => {
+    divImg.style.border = '6px solid black';
+})
+
+function pMouse(){
+    divImg.style.border = '2px solid black';
+}
+
+function sMouse(){
+    divImg.style.border = 'none';
+}
+
+divImg.addEventListener('mouseover', pMouse);
+divImg.addEventListener('mouseout', sMouse)
+
+p.onclick = ()=> {
+    alert('Parágrafo clicado!')
+}
+
+function pressTecla(){
+    if (window.getComputedStyle(divImg, null).getPropertyValue
+    ('background-color')=== 'rgb(0,0,0') {
+        divImg.style.backgroundColor = 'rgb(255, 255, 255)'
+    } else {
+        divImg.style.backgroundColor = 'rgb(0, 0, 0)'
+    };
+
+}
+
+const clique = () => {
+    console.log('Botão clicado!')
+    alert('Botão clicado!')
+}
