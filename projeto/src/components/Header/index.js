b693b55e381a11886cb4494af8296cf1ff3818d5
luanavitori <luanavha2007@gmail.com>
2024-11-13 08:29:05 -0400

    aaa

--- projeto/src/components/Header/index.js
+++ projeto/src/components/Header/index.js
@@ -7,10 +7,10 @@ export default function Header() {
         <header>
             <nav>
                <ul>
-               <li><Link href='/'>Home</Link></li>
-                <li><Link href='/sobre'>Sobre</Link></li>
-                <li><Link href='/contato'>Contato</Link></li>
-               </ul>
+                   <li><Link href='/'>Home</Link></li>
+                   <li><Link href='/sobre'>Sobre</Link></li>
+                   <li><Link href='/contato'>Contato</Link></li>
+                </ul>
             </nav>
         </header>
     )
