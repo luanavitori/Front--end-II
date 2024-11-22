b3d9e1af550fb06b59d449b1a3490b36c37d625e
luanavitori <luanavha2007@gmail.com>
2024-11-22 07:54:25 -0400

    aaa

--- projeto/src/components/Header/index.js
+++ projeto/src/components/Header/index.js
@@ -1,17 +1,25 @@
+'use client';
 import Link from "next/link";
 import styles from "./header.module.css"
+import { useState } from "react";
 
 
 export default function Header() {
+    const [showMenu, setShowMenu] = useState(true)
     return (
         <header>
-            <nav>
-               <ul>
-                   <li><Link href='/'>Home</Link></li>
-                   <li><Link href='/sobre'>Sobre</Link></li>
-                   <li><Link href='/contato'>Contato</Link></li>
-                </ul>
-            </nav>
+            <button onClick={()=>setShowMenu(!showMenu)}>Clique</button>
+            {
+                showMenu &&
+
+                <nav>
+                    <ul>
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/sobre'>Sobre</Link></li>
+                        <li><Link href='/contato'>Contato</Link></li>
+                    </ul>
+                </nav>
+            }
         </header>
     )
 } 
